fceec85bf5f557abf857b4a884e7f0382419f746
hitmands Giuseppe Mandato <gius.mand.developer@gmail.com>
2017-05-10 19:14:17 +0100

    build with a new version code

--- build/GulpSassPedigree.js
+++ build/GulpSassPedigree.js
@@ -1,6 +1,6 @@
 /**!
  ** @name gulp-sass-pedigree
- ** @version 2.1.6
+ ** @version 2.1.7
  ** @author Giuseppe Mandato <gius.mand.developer@gmail.com> (https://github.com/hitmands)
  ** @contributors ["Luca Volta <luca.volta@gmail.com> (https://github.com/lucavolta)"]
  ** @url https://github.com/hitmands/gulp-sass-pedigree#readme
